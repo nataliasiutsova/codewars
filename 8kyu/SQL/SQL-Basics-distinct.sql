d44c249f85137d5d6754ead8991f8462635a1e64
nataliasiutsova <tashaklimenkova@gmail.com>
2023-07-10 23:26:05 +0300

    Added SQL-Basics-distinct.sql

---
+++ 8kyu/SQL/SQL-Basics-distinct.sql
@@ -0,0 +1,14 @@
+/* DESCRIPTION:
+For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+
+people table schema
+id
+name
+age
+
+select table schema
+age (distinct)
+
+SOLUTION:*/
+
+SELECT DISTINCT age FROM people;
