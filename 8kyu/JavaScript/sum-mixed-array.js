30922419c4e5fb41056250244f85bed9ef286f4f
nataliasiutsova <tashaklimenkova@gmail.com>
2023-07-11 18:27:03 +0300

    Added sum-mixed-array.js

---
+++ 8kyu/JavaScript/sum-mixed-array.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+// SOLUTION:
+
+function sumMix(x) {
+  return x.reduce((sum, item) => sum + +item, 0);
+}
